dcf89502c3a1e1dfc9af1e5586ec83066d53252e
Eeley Consulting <111640527+eeleyconsulting@users.noreply.github.com>
2022-08-23 09:54:25 +0200

    Add files via upload

---
+++ python authenticator.py
@@ -0,0 +1,10 @@
+import getpass
+database = {"eeley.consulting": "13579", "consulting.eeley": "97531"}
+username = input("Enter Your Username : ")
+password = getpass.getpass("Enter Your Password : ")
+for i in database.keys():
+    if username == i:
+        while password != database.get(i):
+            password = getpass.getpass("Enter Your Password Again : ")
+        break
+print("Verified")
